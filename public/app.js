d2912bd7b98134fcfa91452ec2b0b402e8ff57a4
Samer Buna <samer.buna@gmail.com>
2015-11-28 15:31:44 -0800

    Pre NG

--- public/app.js
+++ public/app.js
@@ -1,26 +1,30 @@
 'use strict';
 
-var myFirebaseRef = new Firebase("https://aoetnhuhaoeu.firebaseio.com");
-var input = $("#item"), container = $("#items");
+// var myFirebaseRef = new Firebase("https://aoetnhuhaoeu.firebaseio.com");
+// var input = $("#item"), container = $("#items");
 
-$("#add").click(function(event) {
-  event.preventDefault();
-  var newRef = myFirebaseRef.push(input.val());
-  console.log(newRef.key());
-});
+// $("#add").click(function(event) {
+//   event.preventDefault();
+//   var newRef = myFirebaseRef.push(input.val());
+//   console.log(newRef.key());
+// });
 
 // myFirebaseRef.on("value", function(snap) {
 //   console.log("value", snap.val());
 // });
 
-myFirebaseRef.on("child_added", function(snap) {
-  console.log("child_added", snap.val());
-  container.append($("<div>", { data: { key: snap.key() }, text: snap.val() , class: "item" }));
-});
+// myFirebaseRef.on("child_added", function(snap) {
+//   console.log("child_added", snap.val());
+//   container.append($("<div>", { id: snap.key(), data: { key: snap.key() }, text: snap.val() , class: "item" }));
+// });
+
+// myFirebaseRef.on("child_removed", function(snap) {
+//   console.log("child_removed", snap.key(), snap.val());
+//   $(`#${snap.key()}`).remove();
+// });
 
 
-container.on("click", ".item", function() {
-  var $e = $(this);
-  myFirebaseRef.child( $e.data("key") ).remove();
-  // Remove the DOM
-});
+// container.on("click", ".item", function() {
+//   var $e = $(this);
+//   myFirebaseRef.child( $e.data("key") ).remove();
+// });
